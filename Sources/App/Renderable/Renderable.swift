7c890d3a281d0443478a199d73fd45c7c11eabf8
ldstreet <ldstreet@me.com>
2018-07-04 17:15:08 -0500

    removed leaf

--- Sources/App/Renderable/Renderable.swift
+++ Sources/App/Renderable/Renderable.swift
@@ -5,17 +5,14 @@
 //  Created by Luke Street on 5/26/18.
 //
 
-import Leaf
+import Vapor
 
 enum RenderableError: Error, Debuggable {
     
-    case badTemplateString(String)
     case badHTMLString(String)
     
     var identifier: String {
         switch self {
-        case .badTemplateString(_):
-            return "badTemplateString"
         case .badHTMLString(_):
             return "badHTMLString"
         }
@@ -23,8 +20,6 @@ enum RenderableError: Error, Debuggable {
     
     var reason: String {
         switch self {
-        case .badTemplateString(let str):
-            return "\(str) could not be converted to data"
         case .badHTMLString(let str):
             return "\(str) could not be converted to data"
         }
@@ -32,17 +27,7 @@ enum RenderableError: Error, Debuggable {
 }
 
 protocol Renderable {
-    func render(with renderer: TemplateRenderer) throws -> Future<View>
     func render() throws -> View
-}
-
-extension Renderable {
-    func render(with renderer: TemplateRenderer) throws -> Future<View> {
-        throw TemplateKitError.init(identifier: "no implementation", reason: "")
-    }
-    func render() throws -> View {
-        throw TemplateKitError.init(identifier: "no implementation", reason: "")
-    }
 }
 
 protocol HTMLed: CustomStringConvertible {
@@ -60,27 +45,4 @@ extension Renderable where Self: HTMLed {
         guard let data = html.data(using: .utf8) else { throw RenderableError.badHTMLString(html) }
         return View(data: data)
     }
-}
-
-//extension Renderable where Self: TemplatePathed {
-//    func render(with renderer: TemplateRenderer) throws -> Future<View> {
-//        return renderer.render(templatePath, self)
-//    }
-//}
-//
-//extension Renderable where Self: Templated {
-//    func render(with renderer: TemplateRenderer) throws -> Future<View> {
-//        guard let data = template.data(using: .utf8) else {
-//            throw(RenderableError.badTemplateString(template))
-//        }
-//        return renderer.render(template: data, self)
-//    }
-//}
-
-protocol Templated {
-    var template: String { get }
-}
-
-protocol TemplatePathed {
-    var templatePath: String { get }
 }
